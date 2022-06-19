60cb3762bbb659b0febc374d506b092702247534
丁俊华 <1483802325@qq.com>
2022-06-19 10:55:52 +0800

    分支循环1

---
+++ Project4/Project4/text.c
@@ -0,0 +1,83 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+
+main()
+{
+	//for (int i = 0; i < 100; i++)
+	//{
+	//	if (i % 2 == 1)
+	//	{
+	//		printf("%d ", i);
+	//	}
+	//}
+
+	//int day = 0;
+	//scanf("%d", &day);
+	//switch (day)
+	//{
+	//case 1:
+	//case 2:
+	//case 3:
+	//case 4:
+	//case 5:
+	//	printf("weekday");
+	//	break;
+	//case 6:
+	//case 7:
+	//	printf("weekend");
+	//	break;
+	//default:
+	//	printf("无效值");
+	//	break;
+	//}
+	//return 0;
+	//int i = 1;
+	//while (i <= 10)
+	//{
+	//	if (5 == i)
+	//	{
+	//		continue;
+	//	}
+	//	printf("%d ", i);
+	//	i++;
+
+	//}
+	//int ch = 0;
+	//char password[20] = { 0 };
+	//printf("请输入你的密码：");
+	//scanf("%d", password);
+	//while ((ch = getchar()) != '\n')
+	//{
+	//	;
+	//}
+	//printf("是否确认密码(Y/N):");
+	//ch = getchar();
+	//if (ch == 'Y')
+	//{
+	//	printf("密码确认！");
+	//}
+	//else if (ch == 'N')
+	//{
+	//	printf("密码失效");
+	//}
+
+	for (int i = 1; i <= 10; i++)
+	{
+		if (i == 5)
+		{
+			continue;
+		}
+		printf("%d ", i);
+	}
+
+
+
+	return 0;
+}
+
+
+
+
+
